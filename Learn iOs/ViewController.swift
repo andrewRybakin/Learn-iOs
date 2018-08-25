9825428a19bbee5e397804a4955a073f3e2cab75
Andrew Rybakin <drnraw@gmail.com>
2018-08-25 14:24:38 +0400

    I'm toooooo lazy to describe all dis changes

--- Learn iOs/ViewController.swift
+++ Learn iOs/ViewController.swift
@@ -8,18 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+	//MARK: Properties
+	@IBOutlet weak var mealNameLabel: UILabel!
+	@IBOutlet weak var mealNameTextField: UITextField!
+	@IBOutlet weak var mealPhoto: UIImageView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		// Handle the text field’s user input through delegate callbacks.
+		mealNameTextField.delegate = self
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
-
+	
+	//MARK: Actions
+	@IBAction func setDefaultLabelText(_ sender: UIButton) {
+		mealNameLabel.text = "Default name"
+	}
+	
+	@IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+		mealNameTextField.resignFirstResponder()
+		// UIImagePickerController is a view controller that lets a user pick media from their photo library.
+		let imagePicker = UIImagePickerController()
+		// Only allow photos to be picked, not taken.
+		imagePicker.sourceType = .photoLibrary
+		imagePicker.delegate = self
+		present(imagePicker, animated: true, completion: nil)
+	}
+	
+	//MARK: UITextFieldDelegate
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		textField.resignFirstResponder()
+		return true
+	}
+	
+	func textFieldDidEndEditing(_ textField: UITextField) {
+		mealNameLabel.text = mealNameTextField.text
+	}
+	
+	//MARK: UIImagePickerControllerDelegate
+	func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+		// Dismiss the picker if the user canceled.
+		dismiss(animated: true, completion: nil)
+	}
+	
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+		// The info dictionary may contain multiple representations of the image. You want to use the original.
+		guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+			fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+		}
+		mealPhoto.image = selectedImage
+		dismiss(animated: true, completion: nil)
+	}
 }
-
